a6d97d77dba9759c0ceea52c4ddcf030ec1ccfb9
Arun <garunalways@gmail.com>
2023-12-18 07:27:50 +0530

    added commit in github workflow, tweaked AQI_scrape file

--- AQI_scrape.py
+++ AQI_scrape.py
@@ -1,40 +1,42 @@
 # Import necessary packages
 import pandas as pd
-import camelot as camelot
+import camelot
 from datetime import datetime
+import cv2
 
-replace_string = datetime.today().strftime('%Y%m%d')
-file = "https://cpcb.nic.in//upload/Downloads/AQI_Bulletin_20220307.pdf"
-file = file.replace('20220307',str(replace_string),1)
+# Date
+today = datetime.today()
+date_str = today.strftime("%Y%m%d")
 
+file = f'data/AQI_Bulletin_{date_str}.pdf'
 
+#file = f'data/AQI_Bulletin_20231217.pdf'
 # Extract tables 
-
 tables = camelot.read_pdf(file, pages='all', strip_text='\n', flag_size=True)
-
 
 # Define headers
 def filter_df(df):
     df.columns = ['City', 'Air Quality', 'Index Value', 'Prominent Pollutant', 'No. of Monitoring Stations']
-    df.reset_index().drop(columns= 'index', axis=1, inplace=True)
+    df.reset_index(drop=True, inplace=True)
     return df
 
 # Even number of dataframes
 table_list = []
 for num, table in enumerate(tables):
-    if  num % 2 == 0:
+    if num % 2 == 0:
         table_df = table.df
-        table_list.append(table_df.drop(0).drop(0, axis=1))
+        table_list.append(table_df.drop(0).drop(0, axis=1, inplace=True))
 
-# Concatenate all dateframes
+# Concatenate all dataframes
 df = pd.concat(table_list)
 
 # Add headers
-df.pipe(filter_df)
+df = df.pipe(filter_df)
 
 # Add date 
-df['Date'] = pd.to_datetime('today').strftime("%d/%m/%Y")
+df['Date'] = date_str
 
 # Append new data with old file
 with open('AQI.csv', 'a') as f:
     df.to_csv(f, header=False, index=False)
+
